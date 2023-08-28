ab21b28bd499ea0f05062d7bb5b4afa7f6e2decd
Harshita Kaushik <harshitakaushik.hk@gmail.com>
2023-08-28 19:41:58 +0530

    SELECT (NULL Case)

--- Customer that are not referred by customer with id = 2.sql
+++ Customer that are not referred by customer with id = 2.sql
@@ -41,13 +41,14 @@ Output:
 
 Error I made : SELECT name FROM Customer WHERE referee_id != 2;
 
-However, this query will only return one result:Zack although there are 4 customers not referred by Jane (including Jane herself). All the customers who were referred by nobody at all (NULL value in the referee_id column) don’t show up. But why?
+/*However, this query will only return one result:Zack although there are 4 customers not referred by Jane 
+(including Jane herself). All the customers who were referred by nobody at all (NULL value in the referee_id column) don’t show up. But why?
 
 ===> Algorithm
+MySQL uses three-valued logic -- TRUE, FALSE and UNKNOWN. Anything compared to NULL evaluates to the third value: UNKNOWN.
+ That “anything” includes NULL itself! That’s why MySQL provides the IS NULL and IS NOT NULL operators to specifically check for NULL.
 
-MySQL uses three-valued logic -- TRUE, FALSE and UNKNOWN. Anything compared to NULL evaluates to the third value: UNKNOWN. That “anything” includes NULL itself! That’s why MySQL provides the IS NULL and IS NOT NULL operators to specifically check for NULL.
-
-Thus, one more condition 'referee_id IS NULL' should be added to the WHERE clause as below.
+Thus, one more condition 'referee_id IS NULL' should be added to the WHERE clause as below.*/
 
 Correct Answer : SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
 
